2d0b68255949d36e079cf56e3bfefe301eca9ed1
strazivari <143390146+strazivari@users.noreply.github.com>
2023-08-29 13:43:29 +0400

    ClockDegreeCalс.java

--- ClockDegreeCalс.java
+++ ClockDegreeCalс.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
-class HelloWorld {
+class ClockDegreeCalc {
     public static void main(String[] args) {
         double h;
         double m;
-        
         Scanner in = new Scanner(System.in);
+
+        do {
         System.out.println("Hours = ...");
         h = in.nextDouble();
-        if (h > 11){
+        if (11 < h && h < 24) {
             h=h-12;
         }
-        System.out.println("Mins = ...");
-        m = in.nextDouble();
+        else if (h > 23 | h < 0) {
+            System.out.println("Error. Try again");
+        }
+    } while(h > 23 | h < 0);
+        do {
+            System.out.println("Mins = ...");
+            m = in.nextDouble();
+            if (m < 0 | m > 59) {
+            System.out.println("Error. Try again");
+        }
+        } while (m < 0 | m > 59);
         
         cornDegree(h, m);
     }
