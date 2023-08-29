7e93045e92f02a4eb00d306a39a2583c87af5a06
strazivari <143390146+strazivari@users.noreply.github.com>
2023-08-29 13:37:23 +0400

    MaxMinAverageArrayValue.java

--- 28-8-23 Task1.java
+++ 28-8-23 Task1.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class HelloWorld {
+class MaxMinAverageArrayValue {
     public static void main(String[] args) {
-        int[] maxMinAve;
+        double[] maxMinAve;
 		int a;
-		int ave = 0;
-		int max;
-		int min;
+		double ave = 0;
+		double max;
+		double min;
+
 		Scanner in = new Scanner(System.in);
-    System.out.println("количество элементов массива?");
-    a = in.nextInt();
-    maxMinAve = new int [a];
+    	System.out.println("Количество элементов массива?");
+    	a = in.nextInt();
+    	maxMinAve = new double [a];
+
             for (int i=0;i<maxMinAve.length;i++){
-                    maxMinAve[i] = (int) ( Math.random() * a);
-                    ave = ave + maxMinAve[i];
+                maxMinAve[i] = ( Math.random());
+                ave = ave + maxMinAve[i];
             }
-    ave = ave / maxMinAve.length;
-    max = Arrays.stream(maxMinAve).max().getAsInt();
-    min = Arrays.stream(maxMinAve).min().getAsInt();
-    System.out.println("Max - " + max);
-    System.out.println("Min - " + min);
-    System.out.println("Average - " + ave);
+    	ave = ave / maxMinAve.length;
+    	
+    	max = Arrays.stream(maxMinAve).max().getAsDouble();
+    	min = Arrays.stream(maxMinAve).min().getAsDouble();
+    	
+    	System.out.println("Max - " + max);
+    	System.out.println("Min - " + min);
+    	System.out.println("Average - " + ave);
     }
 }
